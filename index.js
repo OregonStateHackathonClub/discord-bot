34b9863152a1fece30ca52efc2518a645988808f
owenkrause <owenjkrause@gmail.com>
2024-10-24 17:43:08 -0700

    setup slash commands and event handlers

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'
-import dotenv from 'dotenv'
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readdirSync } from 'fs';
+import dotenv from 'dotenv';
 
 dotenv.config()
 const token = process.env.DISCORD_TOKEN
-console.log(token)
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+client.commands = new Collection();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const commandsPath = join(__dirname, 'commands');
+const commandFiles = readdirSync(commandsPath);
+
+for (const file of commandFiles) {
+	const filePath = join(commandsPath, file);
+	const command = await import(filePath);
+
+	if ('data' in command.default && 'execute' in command.default) {
+		client.commands.set(command.default.data.name, command.default);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+const eventsPath = join(__dirname, 'events');
+const eventFiles = readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = join(eventsPath, file);
+	const event = await import(filePath);
+
+	if (event.default.once) {
+		client.once(event.default.name, (...args) => event.default.execute(...args));
+	} else {
+		client.on(event.default.name, (...args) => event.default.execute(...args));
+	}
+}
+
 
 client.login(token);
