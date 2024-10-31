9da5af7809c8a2b3780980f6db34199163cdec59
owenkrause <owenjkrause@gmail.com>
2024-10-30 21:15:40 -0700

    some little things

--- packages/bot/src/deploy-commands.js
+++ packages/bot/src/deploy-commands.js
@@ -1,19 +1,20 @@
-import { REST, Routes } from 'discord.js';
-import { dirname, join } from 'path';
+import fs from 'fs';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { readdirSync } from 'fs';
+import { REST, Routes } from 'discord.js';
+
 import dotenv from 'dotenv';
 
 dotenv.config()
 
 const commands = [];
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const commandsPath = join(__dirname, 'commands');
-const commandFiles = readdirSync(commandsPath);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath);
 
 for (const file of commandFiles) {
-    const filePath = join(commandsPath, file);
+    const filePath = path.join(commandsPath, file);
     const command = await import(filePath);
     if ('data' in command.default && 'execute' in command.default) {
         commands.push(command.default.data.toJSON());
