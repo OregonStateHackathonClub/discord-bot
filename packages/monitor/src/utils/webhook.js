8fa490e895df3c34580e3f174aa2c024debf281a
owenkrause <owenjkrause@gmail.com>
2024-11-04 21:59:51 -0800

    forgot semi colon

--- packages/monitor/src/utils/webhook.js
+++ packages/monitor/src/utils/webhook.js
@@ -18,7 +18,7 @@ export default hackathon => {
             { name: 'Host', value: hackathon.organization_name, inline: false },
             { name: 'Theme', value: themes.join(", "), inline: true },
             { name: 'Prize', value: `$${hackathon.prize_amount.replace(/[^\d,]/g, '')}`, inline: true },
-        )
+        );
     
     (new WebhookClient({ url: webhook })).send({ embeds: [embed] })
   }
